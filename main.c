ee001658ab10866b7d6251b72f8a6498ba828dc2
ansu <amitanshurajneti@gmail.com>
2023-11-25 02:56:27 +0530

    added sort

--- main.c
+++ main.c
@@ -8,8 +8,9 @@
 
 static char Prompt[] = "[username ]"; // + The current directory
 static char CurrentDirectory[100] = {0};
+static char** line;
 
-static char* Commands[] = {"cd", "exit", "remove", "pwd", "mkdir", "rmdir", "wc"}; // all the commands
+static char* Commands[] = {"cd", "exit", "remove", "pwd", "mkdir", "rmdir", "wc", "sort"}; // all the commands
 
 // functions for respective commands
 int my_cd(char**);
@@ -19,6 +20,7 @@ int my_pwd();
 int my_mkdir(char**);
 int my_rmdir(char**);
 int my_wc(char**);
+int my_sort(char**); 
 
 char** ParseCommand(char*);
 int ExecuteCommand(char**);
@@ -29,7 +31,7 @@ void ShellLoop();
 char* GetInput();
 void PrintArgs(char**);
 
-int (*BuiltInCommands[]) (char**) = { // the commands should be in order in which they are defined
+int (*BuiltInCommands[]) (char**) = { // the commands should be in order in which they are defined in Commands
     my_cd,   
     my_exit,
 	my_remove,
@@ -37,6 +39,7 @@ int (*BuiltInCommands[]) (char**) = { // the commands should be in order in whic
 	my_mkdir,
 	my_rmdir,
 	my_wc,
+	my_sort,
 };
 
 int main(int argc, char* argv[]) {
@@ -89,7 +92,7 @@ char* GetInput() {
 
 
 int ExecuteCommand(char** args) {
-    for (int i = 0; i < 7; ++i) {
+    for (int i = 0; i < 8; ++i) {
         if (strcmp(Commands[i], args[0]) == 0) {
             (BuiltInCommands[i])(args);
         }
@@ -151,4 +154,46 @@ int my_wc(char** args) {
 
 	printf("The number of lines, words and bytes in the file %s are %d %d %d respectively\n", args[1], lineCount, wordCount, byteCount);
 	return 1;
+}
+
+int my_sort(char** args) {
+    printf("Sorting");
+	FILE* file = fopen(args[1], "r");
+    char line_buf[300];
+    long int size = ftell(file);
+
+    line = (char**)malloc(size);
+
+    if (file == NULL) {
+        printf("File not found");
+        return 1;
+    }
+
+    int x = 0;
+
+    while (fgets(line_buf, sizeof(line_buf), file) != NULL) {
+        char* newLine = strdup(line_buf);
+        line[x] = newLine;
+        printf("%s", line[x]);
+        x++;
+    }
+
+// x++;
+
+    //sorting
+    for (int i = 0; i < x; i++) {
+       for (int j = 0; j < x; j++) {
+           if (strcmp(line[i], line[j]) <= 0){
+               char* temp = line[i];
+               line[i] = line[j];
+               line[j] = temp;
+           }
+       }
+    }
+
+    for (int i = 0; i < x; i++) {
+        printf("%s", line[i]);
+    }
+
+    
 }
