79f5436dc0201a19d7caccd87f4784d7bce2fc49
amitansu1024 <amitanshurajneti@gmail.com>
2023-11-28 00:00:53 +0530

    fixed newline bug and improved sorting

--- main.c
+++ main.c
@@ -34,28 +34,28 @@ void PrintArgs(char**);
 int (*BuiltInCommands[]) (char**) = { // the commands should be in order in which they are defined in Commands
     my_cd,   
     my_exit,
-	my_remove,
-	my_pwd,
-	my_mkdir,
-	my_rmdir,
-	my_wc,
-	my_sort,
+    my_remove,
+    my_pwd,
+    my_mkdir,
+    my_rmdir,
+    my_wc,
+    my_sort,
 };
 
 int main(int argc, char* argv[]) {
     InitShell();
-	ShellLoop();
+    ShellLoop();
     
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 
 void InitShell() {
-	if ((getcwd(CurrentDirectory, 100) == 0))
-		fprintf(stderr, "Failed to initialize directory");
+    if ((getcwd(CurrentDirectory, 100) == 0))
+	fprintf(stderr, "Failed to initialize directory");
 }
 
 void DisplayPrompt() {
-	printf("%s%s ~ ", Prompt, CurrentDirectory);
+    printf("%s%s ~ ", Prompt, CurrentDirectory);
 }
 
 char** ParseCommand(char* line) {
@@ -64,6 +64,16 @@ char** ParseCommand(char* line) {
 
     int i = 0;
     while (token != NULL) {
+	int j = 0;
+
+	while (token[j] != '\0') {
+	    if (token[j] == '\n') {
+		token[j] = '\0';
+		break;
+	    }
+	    j++;
+	}
+
         args[i] =  token;
         i++;
         token = strtok(NULL, " ");
@@ -72,93 +82,97 @@ char** ParseCommand(char* line) {
 }
 
 void ShellLoop() {
-	while (1)  {
-		DisplayPrompt();
-		char* input = GetInput();
-		char** args = ParseCommand(input);
-        ExecuteCommand(args);
-	}
+    while (1)  {
+	DisplayPrompt();
+	char* input = GetInput();
+	char** args = ParseCommand(input);
+	ExecuteCommand(args);
+    }
 }
 
 char* GetInput() {
-	char *input = {0};
-	ssize_t bufsize = 0;
+    char *input = {0};
+    ssize_t bufsize = 0;
 
-	if (getline(&input, &bufsize, stdin) == -1)  {
-		fprintf(stderr, "Unable to get the input\n");
-	}
-	return input;
+    if (getline(&input, &bufsize, stdin) == -1)  {
+	fprintf(stderr, "Unable to get the input\n");
+    }
+    return input;
 }
 
 
 int ExecuteCommand(char** args) {
+    int commandFound = 0;
     for (int i = 0; i < 8; ++i) {
         if (strcmp(Commands[i], args[0]) == 0) {
             (BuiltInCommands[i])(args);
+	    commandFound = 1;
         }
     } 
+
+    if (!commandFound)
+	printf("%s: No such Command\n", args[0]);
 }
 
 int my_cd(char** args) {
-	printf("%s", args[1]);
     if (chdir(args[1]) == 0) 
-		getcwd(CurrentDirectory, 100);
-	else printf("not successful\n");
-	printf("%s", CurrentDirectory);
+	getcwd(CurrentDirectory, 100);
+    else printf("No such Directory\n");
+
+    printf("%s\n", CurrentDirectory);
 }
 
 
 int my_exit() {
-	printf("Exit\n");
-	exit(EXIT_SUCCESS);
+    printf("Exit\n");
+    exit(EXIT_SUCCESS);
 }
 
 int my_remove(char** args)  {
-	if (remove(args[1]) == 0)
-        return 0;
+    if (remove(args[1]) == 0)
+    return 0;
     return 1;
 }
 
 int my_pwd() {
-	printf("%s\n", CurrentDirectory);
-	return 0;
+    printf("%s\n", CurrentDirectory);
+    return 0;
 }
 
 int my_mkdir(char** args) {
-	mkdir(args[1], 0777);
-	return 0;
+    mkdir(args[1], 0777);
+    return 0;
 }
 
 int my_rmdir(char** args) {
-	rmdir(args[1]);
-	return 0;
+    rmdir(args[1]);
+    return 0;
 }
 
 
 int my_wc(char** args) {
-	FILE* file = fopen(args[1], "r");
-	if (file == NULL) {
-		printf("Couldn't open file\n");
-		return 0;
-	}
+    FILE* file = fopen(args[1], "r");
+    if (file == NULL) {
+	printf("Couldn't open file\n");
+	return 0;
+    }
 
-	int lineCount = 0, wordCount = 0, byteCount = 0;
-	for (char c = getc(file); c != EOF; c = getc(file)) {
-		byteCount++;
-		if (c == '\n')
-			lineCount += 1;
+    int lineCount = 0, wordCount = 0, byteCount = 0;
+    for (char c = getc(file); c != EOF; c = getc(file)) {
+	byteCount++;
+	if (c == '\n')
+	    lineCount += 1;
 
-		if (c == ' ')
-			wordCount++;
-	}
+	if (c == ' ')
+	    wordCount++;
+    }
 
-	printf("The number of lines, words and bytes in the file %s are %d %d %d respectively\n", args[1], lineCount, wordCount, byteCount);
+    printf("The number of lines, words and bytes in the file %s are %d %d %d respectively\n", args[1], lineCount, wordCount, byteCount);
 	return 1;
 }
 
 int my_sort(char** args) {
-    printf("Sorting");
-	FILE* file = fopen(args[1], "r");
+    FILE* file = fopen(args[1], "r");
     char line_buf[300];
     long int size = ftell(file);
 
@@ -174,19 +188,16 @@ int my_sort(char** args) {
     while (fgets(line_buf, sizeof(line_buf), file) != NULL) {
         char* newLine = strdup(line_buf);
         line[x] = newLine;
-        printf("%s", line[x]);
         x++;
     }
 
-// x++;
-
     //sorting
     for (int i = 0; i < x; i++) {
-       for (int j = 0; j < x; j++) {
-           if (strcmp(line[i], line[j]) <= 0){
-               char* temp = line[i];
-               line[i] = line[j];
-               line[j] = temp;
+       for (int j = 0; j < x - 1; j++) {
+           if (strcmp(line[j], line[j + 1]) >= 0){
+               char* temp = line[j];
+               line[j] = line[j + 1];
+               line[j + 1] = temp;
            }
        }
     }
@@ -194,6 +205,4 @@ int my_sort(char** args) {
     for (int i = 0; i < x; i++) {
         printf("%s", line[i]);
     }
-
-    
 }
